3358e91cf281b3d23838e183fb02077e5b3d08c9
Daniel <adeosundaniel33@gmail.com>
2023-02-07 15:24:25 +0100

    spinner layout with spinner gif

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -2,8 +2,8 @@ import spinner from '../layout/spinner.gif'
 
 function Spinner() {
   return (
-    <div className='w-100 mt-20'>
-      <img className='text-center mx-auto' width={180} src={spinner} alt="Loading..." />
+    <div className='w-100 mt-10 dark:bg-slate-900'>
+      <img className='text-center mx-auto' width={50} src={spinner} alt="Loading..." />
     </div>
   )
 }
