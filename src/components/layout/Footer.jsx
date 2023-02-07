04141043d72a80e9fb6871d4f74c2809d48f4af0
Daniel <adeosundaniel33@gmail.com>
2023-02-07 15:23:42 +0100

    Footer layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,12 @@
+import {FaGithub} from 'react-icons/fa'
 function Footer() {
 const footerYear = new Date().getFullYear()
   return (
-    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center dark:bg-slate-900">
     <div className="">
         <svg></svg>
-        <p>Copyright &copy; {footerYear} All rights reserved</p>
+        <FaGithub className="text-3xl"/>
+        <p>Built by:  <span className='text-indigo-600'>Daniel </span> Copyright &copy; {footerYear} All rights reserved</p>
     </div>
     </footer>
   )
