7f931ccc106629620e70afdd34ee2c40a280660f
Daniel <adeosundaniel33@gmail.com>
2023-02-07 15:10:49 +0100

    github reducer

--- src/components/context/github/GithubReducer.js
+++ src/components/context/github/GithubReducer.js
@@ -23,6 +23,35 @@ const githubReducer = (state, action) => {
                 ...state,
                 users: []
             }
+            case 'LOAD_TRUE': 
+            return {
+                ...state,
+                loading: true
+            }
+        case 'LOAD_FALSE': 
+            return {
+                ...state,
+                loading: false
+            }
+        case 'CLEAR': 
+            return {
+                ...state,
+                loading: false,
+                users: [],
+            }
+
+        case 'GET_USER':
+            return {
+                ...state,
+                user: action.payload,
+                loading: false
+            }
+        case 'GET_REPOS':
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
+            }
             
         default:
             return state
