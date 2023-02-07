c32dcdd1c65fa5da8e5c81d642f5b689381ff6ae
Daniel <adeosundaniel33@gmail.com>
2023-02-07 15:21:27 +0100

    navbar layout component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,8 +1,9 @@
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
- 
+import DarkMode from "../../utils/DarkMode"
 function Navbar({ title }) {
+  const [colorScheme, setTheme] = DarkMode();
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
@@ -21,6 +22,26 @@ function Navbar({ title }) {
             <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
               About
             </Link>
+            {/* <Link to="/" className="text-lg font-bold align-middle text-black">
+                    <span className="text-indigo-600">GitHub </span> 
+                    <span className="text-gray-800 dark:text-gray-200">
+                        Tracker
+                    </span>
+                </Link> */}
+            <div>
+                <span onClick={() => setTheme(colorScheme)} className='w-10 h-10 bg-indigo-600 rounded-full shadow-lg cursor-pointer text-white flex items-center justify-center'>
+                    {colorScheme === 'light' ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="#fff" viewBox="0 0 24 24" stroke="#fff" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg> 
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#fff" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                    )}
+                     
+                </span>
+            </div>
                     </div>
         </div>
       </div>
